0d5e46fe67d02ca2a2c7259e1d282486606d2802
Mitchell Jurich <mdj50790@ucmo.edu>
2019-03-29 22:03:11 -0500

    Added bullet spawning to a weapon attached to the player. The bullets are currently physics based rigidbodies that are given Forces from the weapon script.

---
+++ Polymorphic/Assets/Player/WeaponController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+	[SerializeField] private GameObject bulletPrefab;
+	[SerializeField] private string shootInputName;
+	[SerializeField] private string destroyInputName;
+	[SerializeField] private float bulletForce;
+
+	private Mesh weaponMesh;
+
+	private void Awake() {
+		weaponMesh = GetComponent<MeshFilter>().mesh;
+	}
+
+    private void Update() {
+		FireWeapon();
+		DestroyBullets();
+    }
+	
+	private void FireWeapon() {
+		if (Input.GetButtonDown(shootInputName)) {
+			// Calculate the spawn position of the bullet so it is is positioned directly at the end of the weapon in the forward direction
+			// Start at origin of weapon
+			Vector3 bulletSpawnPos = transform.position;
+			// Half the width of the weapon in the direction of the Z axis = the mesh extent in z * any scaling to the transform
+			float weaponForwardHalfWidth = weaponMesh.bounds.extents.z * transform.localScale.z;
+			float bulletForwardHalfWidth = bulletPrefab.GetComponent<MeshFilter>().sharedMesh.bounds.extents.z * bulletPrefab.transform.localScale.z;
+			// Add a forward vecor with magnitude equal to half the width of the weapon and bullet z axes in their local spaces
+			bulletSpawnPos += transform.forward * (weaponForwardHalfWidth + bulletForwardHalfWidth);
+
+			GameObject newBullet = Instantiate(bulletPrefab, bulletSpawnPos, transform.rotation) as GameObject;
+			newBullet.SetActive(true);
+			newBullet.GetComponent<Rigidbody>().AddForce(transform.forward * bulletForce);
+		}
+	}
+
+	private void DestroyBullets() {
+		if (Input.GetButtonDown(destroyInputName)) {
+			GameObject[] bullets = GameObject.FindGameObjectsWithTag("Bullet");
+			foreach (GameObject obj in bullets)
+				GameObject.Destroy(obj);
+		}
+	}
+}
